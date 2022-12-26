5c10ff9e3eabd6ffafcde39b33acf46d58cb84c6
d4rkk3y <vuhoanhuy99@gmail.com>
2022-12-26 20:37:37 +0700

    First commit

---
+++ check_price.py
@@ -0,0 +1,34 @@
+from datetime import datetime
+import pytz
+import scrapy
+from scrapy.crawler import CrawlerProcess
+import re
+import csv
+
+class SanpinetworkSpider(scrapy.Spider):
+    name = "sanpinetwork"
+    start_urls = [
+        'https://sanpinetwork.com/'
+    ]
+
+    def parse(self, response):
+        data = []
+        utc_datetime = datetime.utcnow()
+        vn_datetime = utc_datetime.astimezone(pytz.timezone('Asia/Ho_Chi_Minh'))
+        intTime = vn_datetime.timestamp()
+        buyPrice = response.selector.xpath('//*[@id="container"]/section[1]/div[1]/ul/li[1]/strong/text()').get()
+        sellPrice = response.selector.xpath('//*[@id="container"]/section[1]/div[1]/ul/li[2]/strong/text()').get()
+        data.append(intTime)
+        data.append(re.sub(r'[^0-9]', '', buyPrice))
+        data.append(re.sub(r'[^0-9]', '', sellPrice))
+        with open('price.csv', 'a', newline='') as file:
+            writer = csv.writer(file, delimiter=',')
+            writer.writerow(data)
+
+if __name__ == "__main__":
+    process = CrawlerProcess({
+        'USER_AGENT': 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1)'
+    })
+
+    process.crawl(SanpinetworkSpider)
+    process.start()
---
+++ generate_graph.py
@@ -0,0 +1,12 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+from datetime import datetime
+
+columns = ['datetime','buy_price','sell_price']
+data = pd.read_csv("price.csv", delimiter=',', header=None, names=columns)
+data['datetime'] = [datetime.fromtimestamp(x) for x in data['datetime']]
+x_col  = 'datetime'
+y_cols = ['buy_price', 'sell_price']
+
+plt.plot(data[x_col], data[y_cols])
+plt.show()
