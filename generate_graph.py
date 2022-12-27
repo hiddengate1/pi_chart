30a1b084443050290927bc472a35e5a8e2dd3fb2
hiddengate1 <38138355+hiddengate1@users.noreply.github.com>
2022-12-27 19:45:17 +0700

    Update generate_graph.py

--- generate_graph.py
+++ generate_graph.py
@@ -4,7 +4,7 @@ from datetime import datetime
 
 data = pd.read_csv("price.csv", delimiter=',', header=None)
 #only use last 50 columns
-data = data.iloc[-50:]
+data = data.iloc[-80:]
 data[0] = [datetime.fromtimestamp(x + 25200) for x in data[0]]
 
 plt.figure(figsize=(19.2, 10.8))
