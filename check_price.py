f6786ac30e22e68d8200960047f54124599eb074
hiddengate1 <38138355+hiddengate1@users.noreply.github.com>
2023-01-04 23:08:50 +0700

    Update check_price.py

--- check_price.py
+++ check_price.py
@@ -1,26 +1,32 @@
 from datetime import datetime
 import re
 import csv
+from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.chrome import ChromeType
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.support.wait import WebDriverWait
+import selenium.webdriver.support.expected_conditions as EC
 import undetected_chromedriver as uc
 from selenium.webdriver.common.by import By
+from pyvirtualdisplay import Display
 
 URL = 'https://sanpinetwork.com/'
 
-options = uc.ChromeOptions()
-user_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.50 Safari/537.36'
-options.add_argument(f'user-agent={user_agent}')
-driver = uc.Chrome(options=options)
-driver.implicitly_wait(10.0)
+display = Display(visible=0, size=(800, 600))
+display.start()
+
+chrome_service = Service(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install())
+chrome_options = uc.ChromeOptions()
+driver = uc.Chrome(service=chrome_service, options=chrome_options)
 driver.get(url=URL)
-driver.get_screenshot_as_file("screenshot.png")
-buyPrice = driver.find_element(by=By.XPATH, value='//*[@id="container"]/section[1]/div[1]/ul/li[1]/strong').text
-sellPrice = driver.find_element(by=By.XPATH, value='//*[@id="container"]/section[1]/div[1]/ul/li[2]/strong').text
+buyPrice = WebDriverWait(driver=driver, timeout=20.0).until(EC.presence_of_element_located((By.XPATH, '//*[@id="container"]/section[1]/div[1]/ul/li[1]/strong'))).text
+sellPrice = WebDriverWait(driver=driver, timeout=20.0).until(EC.presence_of_element_located((By.XPATH, '//*[@id="container"]/section[1]/div[1]/ul/li[2]/strong'))).text
 data = []
 utc_datetime = datetime.utcnow()
 intTime = utc_datetime.timestamp()
 data.append(intTime)
 data.append(re.sub(r'[^0-9]', '', buyPrice))
 data.append(re.sub(r'[^0-9]', '', sellPrice))
-with open('price2.csv', 'a', newline='') as file:
+with open('price.csv', 'a', newline='') as file:
     writer = csv.writer(file, delimiter=',')
     writer.writerow(data)
