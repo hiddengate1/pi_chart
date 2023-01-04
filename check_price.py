636045283be48f43888906a3261b6e8540de59cd
hiddengate1 <38138355+hiddengate1@users.noreply.github.com>
2023-01-04 22:14:28 +0700

    Update check_price.py

--- check_price.py
+++ check_price.py
@@ -1,33 +1,26 @@
 from datetime import datetime
-import pytz
-import scrapy
-from scrapy.crawler import CrawlerProcess
 import re
 import csv
+import undetected_chromedriver as uc
+from selenium.webdriver.common.by import By
 
-class SanpinetworkSpider(scrapy.Spider):
-    name = "sanpinetwork"
-    start_urls = [
-        'https://sanpinetwork.com/'
-    ]
+URL = 'https://sanpinetwork.com/'
 
-    def parse(self, response):
-        data = []
-        utc_datetime = datetime.utcnow()
-        intTime = utc_datetime.timestamp()
-        buyPrice = response.selector.xpath('//*[@id="container"]/section[1]/div[1]/ul/li[1]/strong/text()').get()
-        sellPrice = response.selector.xpath('//*[@id="container"]/section[1]/div[1]/ul/li[2]/strong/text()').get()
-        data.append(intTime)
-        data.append(re.sub(r'[^0-9]', '', buyPrice))
-        data.append(re.sub(r'[^0-9]', '', sellPrice))
-        with open('price.csv', 'a', newline='') as file:
-            writer = csv.writer(file, delimiter=',')
-            writer.writerow(data)
-
-if __name__ == "__main__":
-    process = CrawlerProcess({
-        'USER_AGENT': 'Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 5.1)'
-    })
-
-    process.crawl(SanpinetworkSpider)
-    process.start()
+options = uc.ChromeOptions()
+user_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.50 Safari/537.36'
+options.add_argument(f'user-agent={user_agent}')
+driver = uc.Chrome(options=options)
+driver.implicitly_wait(10.0)
+driver.get(url=URL)
+driver.get_screenshot_as_file("screenshot.png")
+buyPrice = driver.find_element(by=By.XPATH, value='//*[@id="container"]/section[1]/div[1]/ul/li[1]/strong').text
+sellPrice = driver.find_element(by=By.XPATH, value='//*[@id="container"]/section[1]/div[1]/ul/li[2]/strong').text
+data = []
+utc_datetime = datetime.utcnow()
+intTime = utc_datetime.timestamp()
+data.append(intTime)
+data.append(re.sub(r'[^0-9]', '', buyPrice))
+data.append(re.sub(r'[^0-9]', '', sellPrice))
+with open('price2.csv', 'a', newline='') as file:
+    writer = csv.writer(file, delimiter=',')
+    writer.writerow(data)
