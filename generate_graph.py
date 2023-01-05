6db2204228a9e7deef42dbb1613a759d7d863897
hiddengate1 <38138355+hiddengate1@users.noreply.github.com>
2023-01-05 19:43:42 +0700

    Update generate_graph.py

--- generate_graph.py
+++ generate_graph.py
@@ -6,8 +6,6 @@ data = pd.read_csv("price.csv", delimiter=',', header=None)
 #only use last 50 columns
 data = data.iloc[-80:]
 data[0] = [datetime.fromtimestamp(x + 25200) for x in data[0]]
-
-plt.figure(figsize=(19.2, 10.8))
-plt.xticks(rotation=60)
-plt.plot(data[0], data[[1,2]])
+data.columns=['Time', 'Buy price', 'Sell price']
+data.plot(kind='line', grid=True, x=0, y=[1,2], xlabel="Time", ylabel="Price(vnd)", figsize=[19.2, 10.8])
 plt.savefig('graph.png')
